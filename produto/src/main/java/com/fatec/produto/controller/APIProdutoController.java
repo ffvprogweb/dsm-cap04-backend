62927fb7f3b7b7f824c56f3ed03ed7ed5672bb14
ffvprogweb <disclabes@gmail.com>
2023-09-02 21:46:10 -0300

    Refatoração - log4g no controller inclusao do 2 pedidos

--- produto/src/main/java/com/fatec/produto/controller/APIProdutoController.java
+++ produto/src/main/java/com/fatec/produto/controller/APIProdutoController.java
@@ -1,8 +1,11 @@
 package com.fatec.produto.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +15,13 @@ import com.fatec.produto.servico.IProdutoServico;
 @RestController
 @RequestMapping("api/v1/produtos")
 public class APIProdutoController {
+	Logger logger = LogManager.getLogger(this.getClass());
 	@Autowired
 	IProdutoServico produtoServico;
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity <Object> consultaTodos(){
+		logger.info(">>>>>> apicontroller consulta todos");
 		return ResponseEntity.status(HttpStatus.OK).body(produtoServico.consultaCatalogo());
 	}
 
