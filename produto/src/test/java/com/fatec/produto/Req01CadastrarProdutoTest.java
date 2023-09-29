b3302f9db30fbd3adbce8b91fac337cd436932a6
ffvprogweb <disclabes@gmail.com>
2023-09-29 11:00:44 -0300

    Refatoração - innclusão dos casos de teste da camada de persistencia

--- produto/src/test/java/com/fatec/produto/Req01CadastrarProdutoTest.java
+++ produto/src/test/java/com/fatec/produto/Req01CadastrarProdutoTest.java
@@ -1,19 +1,69 @@
 package com.fatec.produto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fatec.produto.model.IProdutoRepository;
 import com.fatec.produto.model.Produto;
-
+@SpringBootTest
 class Req01CadastrarProdutoTest {
-
+	@Autowired
+	IProdutoRepository repository;
 	@Test
-	void ct01_cadastra_produto_com_sucesso_custo() {
-		Produto produto = new Produto();
-		produto.setCusto(12);
-		assertEquals(12, produto.getCusto());
+	void ct01_cadastrar_produto_com_sucesso() {
+		//dado que nao existe nenhum produto cadastrado
+		repository.deleteAll();
+		//quando o usuario cadastrar um produto
+		Produto produto1 = new Produto("Eletrobomba 110V para Maquina de Lavar e Lava Louças", "maquina de lavar", 51.66, 12);
+		Produto p =repository.save(produto1);
+		//entao o produto fica disponivel para consulta
+		assertEquals(1, repository.count());
+		assertNotNull(p);
+		assertTrue(p.equals(produto1));
 
 	}
-
+	@Test
+	void ct02_cadastrar_produto_com_descricao_invalida_vazio() {
+		//dado que nao existe nenhum produto cadastrado
+		repository.deleteAll();
+		//quando o usuario cadastrar um produto com descricao invalida
+		//isblank = true isempty = true
+		try {
+			Produto produto1 = new Produto("", "maquina de lavar", 51.66, 12);
+			fail("Deveria falhar descricao invalida");
+		} catch (IllegalArgumentException e) {
+			assertEquals ("A descrição não deve estar em branco", e.getMessage());
+		}
+	}
+	@Test
+	void ct03_cadastrar_produto_com_descricao_invalida_null() {
+		//dado que nao existe nenhum produto cadastrado
+		repository.deleteAll();
+		//quando o usuario cadastrar um produto com descricao invalida
+		try {
+			Produto produto1 = new Produto(null, "maquina de lavar", 51.66, 12);
+			fail("Deveria falhar descricao invalida");
+		} catch (IllegalArgumentException e) {
+			assertEquals ("A descrição não deve estar em branco", e.getMessage());
+		}
+	}
+	@Test
+	void ct04_cadastrar_produto_com_descricao_invalida_em_branco() {
+		//dado que nao existe nenhum produto cadastrado
+		repository.deleteAll();
+		//quando o usuario cadastrar um produto com descricao invalida
+		//um string com espacos em branco is.blank=true mas não eh vazio is.empty=false
+		try {
+			Produto produto1 = new Produto("   ", "maquina de lavar", 51.66, 12);
+			fail("Deveria falhar descricao invalida");
+		} catch (IllegalArgumentException e) {
+			assertEquals ("A descrição não deve estar em branco", e.getMessage());
+		}
+	}
 }
